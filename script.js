21b8fd3189117cf0781c69502fad303a86c22657
Thompson Daniel <thompsonacedaniel@gmail.com>
2026-05-01 13:27:51 +0100

    sidebar commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const yearEl = document.getElementById("year");
 const themeSwitcher = document.getElementById("theme-switcher");
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.querySelector(".sidebar");
 
 const date = new Date();
 const year = date.getFullYear();
 yearEl.textContent = year;
 
 themeSwitcher.onclick = () => {
-  const themeIcon = document.getElementById("themeIcon")
+  const themeIcon = document.getElementById("themeIcon");
   if (document.documentElement.classList.contains("dark")) {
     themeIcon.innerHTML = "dark_mode";
     document.documentElement.classList.remove("dark");
@@ -14,4 +16,10 @@ themeSwitcher.onclick = () => {
     themeIcon.innerHTML = "light_mode";
     document.documentElement.classList.add("dark");
   }
+};
+
+menuBtn.onclick = () => {
+  sidebar.classList.contains("sidebar-open")
+    ? sidebar.classList.remove("sidebar-open")
+    : sidebar.classList.add("sidebar-open");
 };
